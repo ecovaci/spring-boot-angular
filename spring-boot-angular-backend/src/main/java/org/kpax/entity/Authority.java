dfa25a519f7058a7caf0da0a96ef2f6b8dc587bd
ecovaci <eugen.covaci.q@gmail.com>
2019-05-08 16:10:44 +0300

    Lombok support

---
+++ spring-boot-angular-backend/src/main/java/org/kpax/entity/Authority.java
@@ -0,0 +1,39 @@
+package org.kpax.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ * An authority (a security role) used by Spring Security.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "authority")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Authority implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long authorityPk;
+
+    @NotNull
+    @Size(max = 50)
+    @Id
+    @Column(length = 50)
+    private String name;
+
+
+}
