2514072fc72421d58ae6a6b863d8f2f974322871
ecovaci <eugen.covaci.q@gmail.com>
2019-05-10 08:46:15 +0300

    Security client

---
+++ spring-boot-angular-frontend/src/main/webapp/src/app/components/login/login.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {User} from "../../model/user";
+import {AuthService} from "../../service/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponent implements OnInit {
+  user: User = new User();
+  errorMessage: string;
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.authenticate(this.user)
+      .then((value) => {
+        this.router.navigate(['home']);
+      }).catch((reason) => {
+      this.errorMessage = reason.error.message;
+    });
+
+
+    /*    (e) => {
+          this.router.navigate(['home']);
+          console.log(e);
+          let resp: any;
+          resp = e.principal;
+          // this.user.fullName = 'ndh';
+          if (resp) {
+            // store user details  in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(resp));
+          }
+        }*/
+
+
+  }
+}
---
+++ spring-boot-angular-frontend/src/main/webapp/src/app/service/auth.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {AuthServerProvider} from "./auth-provider.service";
+import {Account} from "../model/account";
+import * as _ from 'lodash';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private router: Router;
+
+  constructor(private http: HttpClient, private authServerProvider: AuthServerProvider) {
+  }
+
+  account: Account = null;
+
+  authenticate(credentials): Promise<Account> {
+    return new Promise((resolve, reject) => {
+      this.authServerProvider.login(credentials).subscribe(
+        data => {
+          this.account = new Account(data.username, _.chain(data.authorities).map(
+            (authority) => authority.authority
+          ).value());
+          resolve(this.account);
+        },
+        err => {
+          console.log("=====", err)
+          this.logout();
+          reject(err);
+        }
+      );
+    });
+
+  }
+
+  isUserLoggedIn() :boolean {
+    return this.account != null;
+  }
+
+  logout() {
+
+  }
+}
