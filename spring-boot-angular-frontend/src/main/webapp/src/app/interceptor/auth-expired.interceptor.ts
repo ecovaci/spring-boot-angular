412f00137c1bcaa11f0e90d8c98949ba8590c8f7
ecovaci <eugen.covaci.q@gmail.com>
2019-05-14 15:33:01 +0300

    Code cleanup.

--- spring-boot-angular-frontend/src/main/webapp/src/app/interceptor/auth-expired.interceptor.ts
+++ spring-boot-angular-frontend/src/main/webapp/src/app/interceptor/auth-expired.interceptor.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { AuthServerProvider } from '../service/auth-provider.service';
-import { EventService } from '../service/event.service';
-import {EventModel} from "../model/event";
+import {Injectable} from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {AuthServerProvider} from '../service/auth-provider.service';
+import {EventService} from '../service/event.service';
+import {EventModel, EventType} from "../model/event";
 
 @Injectable()
 export class AuthExpiredInterceptor implements HttpInterceptor {
@@ -21,7 +21,7 @@ export class AuthExpiredInterceptor implements HttpInterceptor {
                     if (err instanceof HttpErrorResponse) {
                         if (err.status === 403 && err.url && !err.url.includes('/api/account')) {
                             this.authServerProvider.logout();
-                            this.eventService.broadcast(new EventModel('login'));
+                            this.eventService.broadcast(new EventModel(EventType.OpenLoginDialog));
                         }
                     }
                 }
